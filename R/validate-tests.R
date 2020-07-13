d1c48f3c969f2e98291a49667ae65719bf5b913d
copernican <copernican@gmail.com>
2020-07-13 17:03:01 -0400

    fix: drop trailing comma

--- R/validate-tests.R
+++ R/validate-tests.R
@@ -174,7 +174,7 @@ run_tests <- function(pkg, test_path = "tests/testthat", root_dir = tempdir(), b
           devtools::install(
             pkg = target_pkg,
             build = build_package,
-            upgrade = "never",
+            upgrade = "never"
             # quiet = TRUE
           )
           devtools::test(
