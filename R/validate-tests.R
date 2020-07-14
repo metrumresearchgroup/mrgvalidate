208c22dd2eeb486186d19faa6fce2e01736de2c4
copernican <copernican@gmail.com>
2020-07-14 12:24:08 -0400

    fix: try running a subset of tests

--- R/validate-tests.R
+++ R/validate-tests.R
@@ -184,6 +184,7 @@ run_tests <- function(pkg, test_path = "tests/testthat", root_dir = tempdir(), b
         {
           devtools::test(
             pkg = target_pkg,
+            filter = "classes",
             reporter = testthat::ListReporter$new()
           )
         },
