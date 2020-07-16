3368e6f5ff5b71bb73db94ee34bac008d6585916
copernican <copernican@gmail.com>
2020-07-16 19:54:58 -0400

    feat: remove install_temp_github_package()

--- R/install.R
+++ R/install.R
@@ -1,21 +1,3 @@
-#' Install a GitHub package to a temporary location
-#'
-#' @inheritParams remotes::install_github
-#'
-#' @return the temporary directory, invisibly
-#' @keywords internal
-install_temp_github_package <- function(repo, ref) {
-  dir <- tempdir()
-  install_with_tests(
-    repo,
-    ref = ref,
-    lib = dir,
-    force = TRUE,
-    quiet = TRUE
-  )
-  invisible(dir)
-}
-
 #' Install a GitHub package with tests
 #'
 #' This is a light wrapper around [remotes::install_github()].
