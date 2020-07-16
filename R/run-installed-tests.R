bef9c29c95809fcf217fcc2cd35709114a1ae501
copernican <copernican@gmail.com>
2020-07-16 18:44:04 -0400

    fix: run_installed_tests() needs non-NULL path

--- R/run-installed-tests.R
+++ R/run-installed-tests.R
@@ -7,40 +7,24 @@
 #'
 #' @return list
 run_installed_tests <- function(package,
-                                path = NULL,
+                                path,
                                 reporter = testthat::ListReporter$new(),
                                 unload = FALSE) {
   checkmate::assert_string(package)
   checkmate::assert_string(path, null.ok = TRUE)
   checkmate::assert_logical(unload, len = 1L)
 
-  # TODO: update to use withr
-  # if (unload) {
-  #   on.exit(
-  #     detach(
-  #       paste0("package:", package),
-  #       unload = TRUE,
-  #       character.only = TRUE
-  #     )
-  #   )
-  # }
+  # TODO: use withr to unload package if it was not previously loaded and is not
+  # also imported
 
-  if (is.null(path)) {
-    res <- testthat::test_package(
-      package,
-      reporter = reporter,
-      stop_on_failure = FALSE
-    )
-  } else {
-    require(package, character.only = TRUE)
-    # this gives the tests access to the package namespace, which is useful if
-    # the package defines constants (for example)
-    env <- list2env(
-      as.list(getNamespace(package), all.names = TRUE),
-      parent = parent.env(getNamespace(package))
-    )
-    # TODO: add package argument once available in CRAN testthat
-    res <- testthat::test_dir(path, reporter = reporter, env = env)
-  }
+  require(package, character.only = TRUE)
+  # this gives the tests access to the package namespace, which is useful if
+  # the package defines constants (for example)
+  env <- list2env(
+    as.list(getNamespace(package), all.names = TRUE),
+    parent = parent.env(getNamespace(package))
+  )
+  # TODO: add package argument once available in CRAN testthat
+  res <- testthat::test_dir(path, reporter = reporter, env = env)
   res
 }
