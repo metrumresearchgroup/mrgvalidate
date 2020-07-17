8e562a473db3241d33535b0ce0c930081e8bc8e5
copernican <copernican@gmail.com>
2020-07-16 20:14:15 -0400

    ci: pass dependencies = FALSE

--- R/test_results.R
+++ R/test_results.R
@@ -24,6 +24,7 @@ test_results <- function(dry_run = TRUE, repo = NULL, ref = NULL, ...) {
     pkg <- install_with_tests(
       repo = repo,
       ref = ref,
+      dependencies = FALSE,
       force = TRUE,
       # quiet = TRUE
     )
