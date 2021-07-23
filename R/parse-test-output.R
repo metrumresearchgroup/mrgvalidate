8a49ae8198b4d4dc75b1474a2078aba097964ec8
Kyle Meyer <kylem@metrumrg.com>
2021-07-23 07:28:33 -0400

    todo: parse_testthat_list_reporter: add some todo comments

--- R/parse-test-output.R
+++ R/parse-test-output.R
@@ -9,12 +9,15 @@ parse_testthat_list_reporter <- function(result) {
       abort(paste("DEV ERROR: parsed more than one test name from results:", paste(.t, collapse = ", ")))
     }
     tibble::tibble(test_name = .t,
+                   # TODO: Collapse these into one variable (e.g., "result")?
                    success = all(map_lgl(.r$results, ~ inherits(.x, "expectation_success"))),
                    fail = any(map_lgl(.r$results, ~ inherits(.x, "expectation_failure"))),
                    skip = any(map_lgl(.r$results, ~ inherits(.x, "expectation_skip")))
     ) %>%
       mutate(
+        # TODO: Call this test_id for consistency with requirements input?
         test_tag = parse_test_tag(test_name),
+        # TODO: It probably makes sense to replace flanking spaces here too.
         test_name = str_replace(test_name, fixed(paste0("[", test_tag, "]")), "")
       )
   })
