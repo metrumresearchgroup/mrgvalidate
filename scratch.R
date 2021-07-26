eff625b7316a9e072ddff4877ba7515555a72aac
Kyle Meyer <kylem@metrumrg.com>
2021-07-26 14:32:33 -0400

    dev: demo: update for upstream sheet changes

--- scratch.R
+++ scratch.R
@@ -2,8 +2,8 @@
 df <- create_validation_docs(
   read_requirements_gsheet(
     "1ScdlEACqC-ypJN1k8m9tjDmFIg1UfzMDyCKOzzOCUTg",
-    title_col = "ReqID", story_col = "Requirement",
-    risk = "Category",  # bogus for now
+    title_col = "ReqID", story_col = "Story Description",
+    risk = "Product Risk",
     test_ids_col = "TestID"),
   "inst/validation-results-sample/"
 )
