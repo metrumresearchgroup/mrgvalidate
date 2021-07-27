9af7e2f6d77b3aa9fe91e9cc7f28a7e026d3a070
Kyle Meyer <kylem@metrumrg.com>
2021-07-27 13:06:59 -0400

    dev: support reading stories
    As with the requirement sheet, it might not be worth keeping the *_col
    parameters for mapping column names to the ones we expect, but add it
    for now since read_requirements_gsheet() still supports it.
    
    The sotries still need to be joined with the requirements, which we'll
    probably want a dedicated helper for.

--- R/read-specs.R
+++ R/read-specs.R
@@ -20,3 +20,32 @@ read_requirements_gsheet <- function
     select(RequirementId, RequirementDescription, TestIds) %>%
     mutate(TestIds = stringr::str_split(TestIds, "[, ]+"))
 }
+
+
+#' Read stories from a Google Sheet.
+#' @param ss,sheet Sheet identifiers passed [googlesheets4::read_sheet()].
+#' @param story_id_col,story_name_col,story_description_col,risk_col,req_ids_col
+#'   Names to remap to StoryId, StoryName, StoryDescription, ProductRisk, and
+#'   RequirementIds columns.
+#' @return Tibble with the above columns.
+#' @export
+read_stories_gsheet <- function
+(
+  ss, sheet = NULL,
+  story_id_col = "StoryId",
+  story_name_col = "StoryName",
+  story_description_col = "StoryDescription",
+  risk_col = "ProductRisk",
+  req_ids_col = "RequirementIds"
+) {
+  dd <- googlesheets4::read_sheet(ss = ss, sheet = sheet)
+  dd %>%
+    rename(StoryId = !!story_id_col,
+           StoryName = !!story_name_col,
+           StoryDescription = !!story_description_col,
+           ProductRisk = !!risk_col,
+           RequirementIds = !!req_ids_col) %>%
+    select(StoryId, StoryName, StoryDescription,
+           ProductRisk, RequirementIds) %>%
+    mutate(RequirementIds = stringr::str_split(RequirementIds, "[, ]+"))
+}
--- scratch.R
+++ scratch.R
@@ -2,5 +2,8 @@
 reqs <- read_requirements_gsheet(
     "12vtANioHgI0BXVnq9nXbpnG1sXJaedVdmxEJsQyWHHU")
 
+stories <- read_stories_gsheet(
+  "1P5PZsFf2OPWhcWqzefBuIheSlzMAXB1kR2_9HUPXIXI")
+
 test_output_dir <- "inst/validation-results-sample"
 df <- create_validation_docs(reqs, test_output_dir)
