cfb5d5619e485ed6699e8b99d3d3ac80061febe5
Kyle Meyer <kylem@metrumrg.com>
2021-07-28 14:26:10 -0400

    chore: delete temporary demo

--- scratch.R
+++
@@ -1,8 +0,0 @@
-
-spec <- read_spec_gsheets(
-  ss_stories = "1P5PZsFf2OPWhcWqzefBuIheSlzMAXB1kR2_9HUPXIXI",
-  ss_req = "12vtANioHgI0BXVnq9nXbpnG1sXJaedVdmxEJsQyWHHU"
-)
-
-test_output_dir <- "inst/validation-results-sample"
-df <- create_validation_docs(spec, test_output_dir)
