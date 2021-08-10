4d715278e7c33ea703199e29f902640513360ce7
seth127 <seth127@gmail.com>
2021-08-10 16:46:42 -0400

    test: added test for running create_validation_docs() without requirements

--- tests/testthat/test-create-validation-docs.R
+++ tests/testthat/test-create-validation-docs.R
@@ -66,11 +66,46 @@ test_that("create_validation_docs() returns data df", {
     auto_test_dir = file.path(TEST_INPUTS_DIR, "validation-results-sample"),
     man_test_dir = file.path(TEST_INPUTS_DIR, "manual-tests-sample"),
     roles = readr::read_csv(file.path(TEST_INPUTS_DIR, "roles.csv"), col_types = "cc"),
-    output_dir = output_dir # CHANGE TO write = FALSE
+    output_dir = output_dir,
+    write = FALSE
   )
 
   expect_true(nrow(res_df) > 1)
   expect_equal(names(res_df), CREATE_OUT_DF_NAMES)
+
+  purrr::walk(names(res_df)[1:ncol(res_df)-1], function(.n) {
+    expect_true(inherits(res_df[[.n]], "character"))
+    expect_true(all(purrr::map_lgl(res_df[[.n]], ~!is.null(.x))))
+    expect_true(all(purrr::map_lgl(res_df[[.n]], ~nchar(.x) > 1)))
+  })
+  expect_true(inherits(res_df$tests, "list"))
+  expect_true(all(purrr::map_lgl(res_df$tests, ~inherits(.x, "tbl_df"))))
+
+})
+
+test_that("create_validation_docs() works with no requirements", {
+  # set up clean docs output dir
+  output_dir <- file.path(tempdir(), "mrgvalidate-create-validation-docs")
+  if (fs::dir_exists(output_dir)) fs::dir_delete(output_dir)
+  fs::dir_create(output_dir)
+  on.exit({ fs::dir_delete(output_dir) })
+
+  res_df <- mrgvalidate::create_validation_docs(
+    product_name = "Metworx TEST",
+    version = "vFAKE",
+    specs = readRDS(file.path(TEST_INPUTS_DIR, "specs.RDS")) %>% select(-RequirementId, -RequirementDescription),
+    auto_test_dir = file.path(TEST_INPUTS_DIR, "validation-results-sample"),
+    man_test_dir = file.path(TEST_INPUTS_DIR, "manual-tests-sample"),
+    roles = readr::read_csv(file.path(TEST_INPUTS_DIR, "roles.csv"), col_types = "cc"),
+    output_dir = output_dir,
+    write = FALSE
+  )
+
+  expect_true(nrow(res_df) > 1)
+  expect_equal(
+    names(res_df),
+    stringr::str_subset(CREATE_OUT_DF_NAMES, "RequirementId|RequirementDescription", negate = TRUE)
+  )
 
   purrr::walk(names(res_df)[1:ncol(res_df)-1], function(.n) {
     expect_true(inherits(res_df[[.n]], "character"))
