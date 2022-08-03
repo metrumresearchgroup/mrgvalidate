7f56daeddaf6e1c3b8a07eae8401277e98854c03
barrettk <barrettk@metrumrg.com>
2022-08-03 17:47:51 -0400

    commiting an old constant to appease drone, but mainly for when I eventually need to remove all the old functions

--- R/aaa.R
+++ R/aaa.R
@@ -9,3 +9,4 @@ TEST_RESULTS_FILE <- "testing-results.Rmd"
 MAT_FILE <- "traceability-matrix.Rmd"
 REQ_FILE <- "requirements-specification.Rmd"
 VAL_SUM_FILE <- "validation-summary.Rmd"
+VAL_FILE <- "validation-testing.md" # keeping this for now to appease drone, and also for when I eventually need to remove all the old functions
